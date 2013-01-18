60c7fbb49438a06a62bdf328816addf9d50ce20c
brolin-laptop <brolin108@gmail.com>
2013-01-17 21:45:20 -0500

    Agrega modelo de Ranking

--- domain/Comment.js
+++ domain/Comment.js
@@ -1,13 +1,8 @@
 var Store = require('../lib/Store');
 
 var CommentStore = Store('Comment', {
-    title: { type: String },
+    comment: { type: String },
     user_id: { type: String },
     mapping_id: { type: String },
-    description: { type: String },
-    lat: { type: Number },
-    lon: { type: Number },
-    video: { type: Array },
-    audio: { type: Array },
-    image: { type: Array }
+    date: { type: Date }
 });
